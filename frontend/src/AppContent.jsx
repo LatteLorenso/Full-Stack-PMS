f8287932e6311d6dc0720425c61b77caaf451632
LatteLorenso <antonreserve67@gmail.com>
2026-06-01 17:25:39 +0500

    Отдельный AppContent.jsx файл для настройки защиты Routes

---
+++ frontend/src/AppContent.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext';
+
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Tasks from './pages/Tasks';
+
+function AppContent() {
+    const { token } = useAuth;
+
+    return (
+        <>
+            {token && <Navbar />}
+
+            <Routes>
+                <Route path='/login' element={<Login />} />
+
+                <Route
+                    path='/'
+                    element={
+                        <PrivateRoute>
+                            <Home />
+                        </PrivateRoute>
+                    }
+                />
+
+                <Route
+                    path='/dashboard'
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+
+                <Route
+                    path='/projects'
+                    element={
+                        <PrivateRoute>
+                            <Projects />
+                        </PrivateRoute>
+                    }
+                />
+
+                <Route
+                    path='/tasks'
+                    element={
+                        <PrivateRoute>
+                            <Tasks />
+                        </PrivateRoute>
+                    }
+                />
+
+                <Route path='*' element={<Login />} />
+            </Routes>
+        </>
+    );
+}
+
+export default AppContent;
