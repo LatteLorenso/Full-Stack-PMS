d836471736a40e8143bc3d59c9f388e89e97e027
LatteLorenso <antonreserve67@gmail.com>
2026-06-05 20:59:22 +0500

    Логика создания папки uploads, если ее нет

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,9 +1,16 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../uploads');
+        const uploadDir = path.join(__dirname, '../../uploads');
+
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
